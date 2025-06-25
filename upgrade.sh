3254b54924a18031f5b85159deecb8f8ca7a4942
pierpaolo <ppagno@gmail.com>
2025-06-25 05:26:59 +0100

    update...

--- upgrade.sh
+++ upgrade.sh
@@ -8,24 +8,24 @@ new_ver=$1
 echo "new version: $new_ver"
 
 # Simulate release of the new docker images
-docker tag nginx:1.23.3 aputra/nginx:$new_ver
+docker tag nginx:1.23.3 ppagnoni/nginx:$new_ver
 
 # Push new version to dockerhub
-docker push aputra/nginx:$new_ver
+docker push ppagnoni/nginx:$new_ver
 
 # Create temporary folder
 tmp_dir=$(mktemp -d)
 echo $tmp_dir
 
 # Clone GitHub repo
-git clone git@github.com:antonputra/lesson-158.git $tmp_dir
+git clone git@github.com:ppagnoni/monnezza.git $tmp_dir
 
 # Update image tag
-sed -i '' -e "s/aputra\/nginx:.*/aputra\/nginx:$new_ver/g" $tmp_dir/my-app/1-deployment.yaml
+sed -i '' -e "s/ppagnoni\/nginx:.*/ppagnoni\/nginx:$new_ver/g" "$tmp_dir/ArgoCD Tutorial for Beginners: GitOps CD for Kubernetes n1/01_example/1-deployment.yaml"
 
 # Commit and push
 cd $tmp_dir
-git add .
+git add -A
 git commit -m "Update image to $new_ver"
 git push
 
